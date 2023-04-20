9541182788687d403e00bc4dd4c21706f3a08733
esanovotabek <esanovotabek688@gmail.com>
2023-04-20 14:18:16 +0500

    test commit

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// salom
 Route::get('/', [SiteController::class, 'index'])->name('index');
 
 Route::get('/login', [AuthController::class, 'index'])->name('login.index');
